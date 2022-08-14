3705379db6fc76c284cb529c2014255ab63ea054
liplum <Li_plum@outlook.com>
2022-08-14 15:19:58 +0800

    Better Block config

--- world/src/plumy/world/Block.kt
+++ world/src/plumy/world/Block.kt
@@ -12,10 +12,43 @@ inline fun <reified B, reified T> Block.config(
                 onConfig(build, v.toInt() as T)
             }
         }
+        Byte::class.java -> {
+            config(java.lang.Byte::class.java) { build: B, v ->
+                onConfig(build, v.toByte() as T)
+            }
+        }
+        Short::class.java -> {
+            config(java.lang.Short::class.java) { build: B, v ->
+                onConfig(build, v.toShort() as T)
+            }
+        }
+        Long::class.java -> {
+            config(java.lang.Long::class.java) { build: B, v ->
+                onConfig(build, v.toLong() as T)
+            }
+        }
+        Float::class.java -> {
+            config(java.lang.Float::class.java) { build: B, v ->
+                onConfig(build, v.toFloat() as T)
+            }
+        }
+        Double::class.java -> {
+            config(java.lang.Double::class.java) { build: B, v ->
+                onConfig(build, v.toDouble() as T)
+            }
+        }
         else -> {
             config(T::class.java) { build: B, v ->
                 onConfig(build, v)
             }
         }
+    }
+}
+
+inline fun <reified B> Block.configNull(
+    crossinline onClear: B.() -> Unit,
+) where B : Building {
+    configClear<B> {
+        it.onClear()
     }
 }
