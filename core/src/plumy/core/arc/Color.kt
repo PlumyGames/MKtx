b6a8c0f84a6d2940b265ceb358e435b7e49adf70
liplum <Li_plum@outlook.com>
2022-08-19 20:38:02 +0800

    Moved `AnimatedColor`. Added `TR.isFound`.

--- core/src/plumy/core/arc/Color.kt
+++ core/src/plumy/core/arc/Color.kt
@@ -1,12 +1,10 @@
 package plumy.core.arc
 
 import arc.graphics.Color
-import arc.util.Time
 import plumy.core.math.Progress
 import plumy.core.math.lerp
 
 val EmptyColor = Color()
-
 private val hsvTemp1 = FloatArray(3)
 private val hsvTemp2 = FloatArray(3)
 /**
@@ -44,38 +42,4 @@ fun Color.lighten(strength: Float): Color {
     g += strength
     b += strength
     return this
-}
-
-class AnimatedColor(
-    val colorSeq: Array<Color>,
-    val duration: Float = 60f,
-    val lerp: LerpType = LerpType.RGB,
-    val useGlobalTime: Boolean = false,
-) {
-    var time = 0f
-        set(value) {
-            field = value.coerceAtLeast(0f)
-        }
-
-    fun spend(delta: Float) {
-        time += delta
-    }
-    /**
-     * Get the current color.
-     *
-     * NOTE: It's an expensive call, please cache the return value.
-     */
-    val color = Color()
-        get() {
-            val time = if (useGlobalTime) Time.globalTime else time
-            val count = (time / duration).toInt() % colorSeq.size
-            val curIndex = count.coerceIn(0, colorSeq.size - 1)
-            val nextIndex = if (curIndex == colorSeq.size - 1) 0 else curIndex + 1
-            val progress = (time % duration) / duration
-            when (lerp) {
-                LerpType.RGB -> field.set(colorSeq[curIndex]).lerp(colorSeq[nextIndex], progress)
-                LerpType.HSV -> field.set(colorSeq[curIndex]).hsvLerp(colorSeq[nextIndex], progress)
-            }
-            return field
-        }
 }
--- core/src/plumy/core/assets/Texture.kt
+++ core/src/plumy/core/assets/Texture.kt
@@ -4,6 +4,7 @@ import arc.graphics.g2d.TextureRegion
 
 typealias TR = TextureRegion
 typealias TRs = Array<TextureRegion>
+
 /**
  * Use this to avoid `lateinit var`
  */
@@ -12,3 +13,6 @@ val EmptyTR = TR()
  * Use this to avoid `lateinit var`
  */
 val EmptyTRs = emptyArray<TR>()
+val TR.isFound get() = texture != null && found()
+val TR.isEmpty get() = texture == null || this == EmptyTR
+val TR.isNonEmpty get() = !isEmpty
